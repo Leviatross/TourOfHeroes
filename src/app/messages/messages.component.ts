e5eea33bb3cd185a2a2a84b24b4a71b052ca52fe
Leviatross <timmonsbrand@gmail.com>
2022-09-05 22:48:28 -0500

    Step 4 completed

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,19 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
 
-@Injectable({
-  providedIn: 'root',
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+
   constructor(public messageService: MessageService) {}
-  
-  messages: string[] = [];
 
-  add(message: string) {
-    this.messages.push(message);
+  ngOnInit() {
   }
 
-  clear() {
-    this.messages = [];
-  }
 }
